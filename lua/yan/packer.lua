1ec515e1a396abc5065570866b951e568f92527f
YanCerqueira <yansilvacerqueira@outlook.com>
2024-02-03 17:55:28 -0300

    feat: add snippets and new packages

--- lua/yan/packer.lua
+++ lua/yan/packer.lua
@@ -26,7 +26,10 @@ return require('packer').startup(function(use)
 	 "hrsh7th/cmp-buffer",
 	 "hrsh7th/cmp-path",
 	 "hrsh7th/cmp-cmdline",
+	 'L3MON4D3/LuaSnip',
+	 'saadparwaiz1/cmp_luasnip',
+	 "rafamadriz/friendly-snippets",
 }
- 
+
  use("theprimeagen/harpoon")
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  LuaSnip = {
+    loaded = true,
+    path = "/home/yancerqueira/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    url = "https://github.com/L3MON4D3/LuaSnip"
+  },
   catppuccin = {
     config = { "\27LJ\2\n@\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0!colorscheme catppuccin-latte\bcmd\bvim\0" },
     loaded = true,
@@ -99,6 +104,16 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/yancerqueira/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
+  },
+  cmp_luasnip = {
+    loaded = true,
+    path = "/home/yancerqueira/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    url = "https://github.com/saadparwaiz1/cmp_luasnip"
+  },
+  ["friendly-snippets"] = {
+    loaded = true,
+    path = "/home/yancerqueira/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    url = "https://github.com/rafamadriz/friendly-snippets"
   },
   harpoon = {
     loaded = true,
